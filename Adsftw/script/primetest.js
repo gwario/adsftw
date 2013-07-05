993feebf84ebcc43ab0b1b08e66bf6fe53886637
Mario Gastegger <mario.gastegger@gmail.com>
2013-07-05 23:54:48 +0200

    add miller rabin prime number test

---
+++ Adsftw/script/primetest.js
@@ -0,0 +1,55 @@
+/**
+ * Implementation of the Miller-Rabin prime number test.
+ */
+(function( adsftw, undefined ) {
+	
+	/**
+	 * Implementation of the Miller-Rabin prime number test.
+	 * Failure probability <= 1/(2^s)
+	 * @param number the number to be tested.
+	 * @param s the number of iterations. the higher the number the higher the certainty.
+	 */
+	adsftw.mr = function(number, s) {
+		
+		if(typeof number === 'number' && typeof s === 'number')
+			return _mr(number, s);
+		else
+			throw new TypeError("need numbers!");
+			
+	};
+	
+	function _mr(number, s) {
+		
+		for(var i = 1, a = 0; i <= s; i++) {
+			a = 1 + Math.floor(Math.random() * number);// [1,number-1]
+			if(witness(a, number))
+				return false;
+		}
+		
+		return true;
+	};
+	
+	/**
+	 * If witness returns true, number is certainly no prime number, otherwise we don't know it exactly.
+	 * 
+	 * @param a a number between 1 and n - 1.
+	 * @param n the number to be tested.
+	 */
+	function witness(a, n) {
+		
+		var b = (n - 1).toString(2);// binary form of n - 1
+		var d = 1;
+		
+		for(var k = b.length - 1; k >= 0; k--) {
+			d = (d * d) % n;
+			if(b[k] === 1)
+				d = (d * a) % n;
+		}
+		
+		if(d !== 1)
+			return true;//n is not primal
+		else
+			return false;
+	};
+	
+}( window.adsftw = window.adsftw || {} ));
